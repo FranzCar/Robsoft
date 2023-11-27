feb0eb492699429f4945304832b545872592fa7f
Franz Carrillo <franzluiz10@gmail.com>
2023-11-26 22:36:38 -0400

    Cambios eventos modificables lista

--- Servidor/proyecto_tis/routes/api.php
+++ Servidor/proyecto_tis/routes/api.php
@@ -32,7 +32,7 @@ Route::post('guardar-participante',[PersonaController::class, 'guardarEstudiante
 Route::get('lista-participantes',[PersonaController::class, 'listParticipantes']);
 //Route::post('guardar-equipo',[EquipoController::class, 'store']);
 Route::get('eventos-modificables', [EventoController::class, 'getEventosEnEspera']);
-//Route::get('lista-equipos',[EquipoController::class, 'listaEquipos']);
+Route::get('lista-equipos',[EquipoController::class, 'listaEquipos']);
 Route::get('lista-organizadores',[RolPersonaController::class, 'listaOrganizadores']);
 Route::get('lista-coach',[RolPersonaController::class, 'listaCoach']);
 Route::get('lista-auspiciadores',[AuspiciadorController::class, 'listaAuspiciadores']);
