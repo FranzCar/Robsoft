e8884f9fecbeb63d7f360a8826e5d3207f63b745
Franz Carrillo <franzluiz10@gmail.com>
2023-12-07 16:39:32 -0400

    Lista coach por institucion

--- Servidor/proyecto_tis/app/Http/Controllers/Api/RolPersonaController.php
+++ Servidor/proyecto_tis/app/Http/Controllers/Api/RolPersonaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\RolPersona;
+use App\Models\Persona;
 use Illuminate\Support\Facades\DB;
 
 class RolPersonaController extends Controller
@@ -38,5 +39,34 @@ class RolPersonaController extends Controller
             WHERE RP.id_roles = 7
             ");
             return response()->json($rolpersona);
+        }
+    public function listaCoachInstitucion(Request $request) {
+            // Obtener el id_institucion del request
+            $idInstitucion = $request->id_institucion;
+        
+            $participantes = Persona::with(['caracteristicasTexto', 'caracteristicasFecha'])
+                                ->whereHas('RolPersona', function($query) {
+                                    $query->where('id_roles', 4);
+                                })
+                                ->when($idInstitucion, function ($query) use ($idInstitucion) {
+                                    return $query->where('id_institucion', $idInstitucion);
+                                })
+                                ->get()
+                                ->map(function ($persona) {
+                                    // Buscar las características específicas
+                                    $fechaNacimiento = $persona->caracteristicasFecha->firstWhere('id_caract_per', 4)->valor_fecha_persona ?? null;
+        
+                                    return [
+                                        'id_persona' => $persona->id_persona,
+                                        'nombre' => $persona->nombre,
+                                        'correo_electronico' => $persona->correo_electronico,
+                                        'telefono' => $persona->telefono,
+                                        'ci' => $persona->ci,
+                                        'genero' => $persona->genero,
+                                        'id_institucion' => $persona->id_institucion,
+                                    ];
+                                });
+        
+            return $participantes;           
         }
 }
